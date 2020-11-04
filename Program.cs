21766ca86af871f39b1810d09936711eead2b018
YuriiTrofimenko <tyaa@ukr.net>
2020-11-04 16:23:41 +0200

    demo

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using CommandSample.ControlledSystems;
+using CommandSample.Commands;
+using System.Collections.Generic;
 
-namespace command
+namespace CommandSample
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var remote = new RemoteControl();
+            string userInput;
+
+            remote.SetCommandForButton(1, new LightsCommand(new Light()));
+            remote.SetCommandForButton(2, new TvCommand(new Tv()));
+            remote.SetCommandForButton(3, new MusicCommand(new Music()));
+            remote.SetCommandForButton(4, new TeapotCommand(new Teapot()));
+
+            var teapotCommand = new TeapotCommand(new Teapot());
+            var tvCommand = new TvCommand(new Tv());
+            var macroCommand = new MacroCommand(new List<ICommand> {teapotCommand, tvCommand});
+            remote.SetCommandForButton(5, macroCommand);
+
+            do
+            {
+                Console.WriteLine("Выберите вариант ниже:");
+                Console.WriteLine(remote);
+
+                Console.Write("\nВаш выбор: ");
+                var input = Console.ReadLine();
+                int buttonId;
+                int.TryParse(input, out buttonId);
+
+                remote.PushButton(buttonId);
+                // remote.UndoButton(buttonId);
+
+                do
+                {
+                  Console.Write("\nВы хотите продолжить (y/n): ");
+                  userInput = Console.ReadLine();
+                } while (userInput != "y" && userInput != "n");
+            } while (userInput == "y");
         }
     }
 }
--- new-console.sh
+++
@@ -1,7 +0,0 @@
-dotnet new console
-dotnet new gitignore
-rm -rf .git
-git init
-git add .
-git commit -a -m"init"
-rm -rf new-console.sh
